9ccebf95068de5a0722bf13812e83d1b5f7a5d09
Julia Brosowski <Julia-Patricia.Brosowski@telekom.de>
2023-08-16 17:04:17 +0200

    symbols quest ready

---
+++ src/Symbols.java
@@ -0,0 +1,20 @@
+public class Symbols {
+    public static void main(String[] args){
+
+        //valid
+        int number = 5;
+        int highNumber1 = 10;
+        int highNumber2 = 20;
+        int lowNumber_1 = 1;
+        int lowNumber_2 = 2;
+
+        //invalid or bad practise
+        //int 1number = 5;
+        //int *Number1 = 10;
+        //int $ = 20;
+        //int _1 = 1;
+        //int 2 = 2;
+
+
+    }
+}
