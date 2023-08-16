0323e1cf139bd67e7cfba04ce1aa960bf199e0e2
Julia Brosowski <Julia-Patricia.Brosowski@telekom.de>
2023-08-16 17:22:40 +0200

    java quest 1

--- src/Senpai.java
+++ src/Senpai.java
@@ -1,6 +1,6 @@
-public class HelloWorld {
+public class Senpai {
     public static void main(String[] args){
         // display Hello World! in the terminal
-        System.out.println("Hello World!");
+        System.out.println("Notice me Senpai");
     }
 }
