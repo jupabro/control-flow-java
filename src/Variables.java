0b3efa68ad144e063697f2ac147b1891114247d2
Julia Brosowski <Julia-Patricia.Brosowski@telekom.de>
2023-08-17 09:50:24 +0200

    variables quest3

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args){
+
+        String movieTitle = "Indiana Jones and the Last Crusade";
+        System.out.println(movieTitle);
+
+        boolean watched = false;
+        System.out.println(watched);
+
+        int yearOfRelease = 1989;
+        System.out.println(yearOfRelease);
+
+        float rating = 8.2f;
+        System.out.println("rating: " + rating);
+    }
+}
